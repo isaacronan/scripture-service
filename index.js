b329137e9ed60749de8ed602180ae504ea2b3240
Isaac Ronan <isaacronan@users.noreply.github.com>
2020-01-31 17:07:45 +0000

    reimplemented jwt signing and auth with passport library

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const passport = require('passport');
+
+const { SECRET } = require('./utils/routing');
+
 const app = express();
 const PORT = 1582;
 
@@ -6,8 +11,13 @@ const books = require('./routes/text');
 const subscriptions = require('./routes/subscriptions');
 const user = require('./routes/user');
 
+app.use(passport.initialize());
 app.use('/books', books);
 app.use('/subscriptions', subscriptions);
 app.use('/user', user);
+
+passport.use(new Strategy({ secretOrKey: SECRET, jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken() }, ({ username }, done) => {
+    done(null, { username });
+}));
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
