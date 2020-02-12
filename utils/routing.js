cb61f5ebbace347983038825d0a8d22279c03e18
Isaac Ronan <isaacronan@charlottes-air.home>
2020-02-12 06:10:19 -0500

    integrated yup for validating subscription request objects

--- utils/routing.js
+++ utils/routing.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const yup = require('yup');
 
 const SECRET = 'secret';
 
@@ -15,45 +16,28 @@ const sign = payload => jwt.sign(payload, SECRET, { expiresIn: 30 * 60 });
 
 const authenticate = passport.authenticate('jwt', { session: false });
 
-const subscriptionFormatIsValid = (subscription) =>
-    !!subscription &&
-    Object.keys(subscription).reduce((acc, key) => acc && [
-        'name',
-        'verseDosage',
-        'bookPool',
-        'currentIssue'
-    ].includes(key), true) && (
-        !subscription.name ||
-        typeof subscription.name === 'string'
-    ) && (
-        !subscription.verseDosage ||
-        typeof subscription.verseDosage === 'number'
-    ) && (
-        !subscription.bookPool ||
-        Array.isArray(subscription.bookPool) &&
-        subscription.bookPool.length > 0 &&
-        subscription.bookPool.reduce((acc, bookNumber) => acc && typeof bookNumber === 'number', true)
-    ) && (
-        !subscription.currentIssue ||
-        issueFormatIsValid(subscription.currentIssue)
-    );
+const updateSubscriptionSchema = yup.object().noUnknown().shape({
+    name: yup.string().min(1),
+    verseDosage: yup.number().integer().positive(),
+    bookPool: yup.array().of(yup.number().integer().positive()).min(1),
+    currentIssue: yup.object().noUnknown().shape({
+        currentBook: yup.number().integer().positive().required(),
+        currentChapter: yup.number().integer().positive().required(),
+        currentVerse: yup.number().integer().positive().required()
+    }).default(undefined)
+});
 
-const issueFormatIsValid = (issue) =>
-    !!issue &&
-    Object.keys(issue).reduce((acc, key) => acc && [
-        'currentBook',
-        'currentChapter',
-        'currentVerse'
-    ].includes(key), true) &&
-    typeof issue.currentBook === 'number' &&
-    typeof issue.currentChapter === 'number' &&
-    typeof issue.currentVerse === 'number';
+const createSubscriptionSchema = yup.object().noUnknown().shape({
+    name: yup.string().min(1).required(),
+    verseDosage: yup.number().integer().positive().required(),
+    bookPool: yup.array().of(yup.number().integer().positive()).min(1).required()
+});
 
 module.exports = {
     SECRET,
     checkResultsAndRespond,
     sign,
     authenticate,
-    subscriptionFormatIsValid,
-    issueFormatIsValid
+    updateSubscriptionSchema,
+    createSubscriptionSchema
 };
