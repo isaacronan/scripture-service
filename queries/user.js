1f7c5967028dd43feaa54b2341ade4f4008346c1
Isaac Ronan <isaacronan@charlottes-air.home>
2020-02-12 20:13:24 -0500

    moved refresh tokens to db

--- queries/user.js
+++ queries/user.js
@@ -1,6 +1,10 @@
+const uuid = require('uuid');
 const { getCollection } = require('../utils/db');
 
 const users = getCollection('users');
+const tokens = getCollection('tokens');
+
+const EXP_TIME_MS = 1 * 60 * 1000;
 
 const getUserAuthInfo = (username) => new Promise(async (resolve) => {
     (await users).find({ username }, { projection: { _id: 0, password: 1 }}).toArray((_err, docs) => {
@@ -13,14 +17,41 @@ const createUserAccount = (username, password) => new Promise(async (resolve) =>
         if (docs.length) {
             resolve(true);
         } else {
-            (await users).insertOne({ username, password }).then((result) => {
+            (await users).insertOne({ username, password }).then(() => {
                 resolve(false);
             });
         }
     });
 });
 
+const createRefreshToken = (username) => new Promise(async (resolve) => {
+    const refresh = uuid.v4();
+    await deleteRefreshTokens(username);
+    (await tokens).insertOne({ username, refresh, timestamp: Date.now() }).then(() => {
+        resolve(refresh);
+    });
+});
+
+const getRefreshToken = (username, refresh) => new Promise(async (resolve) => {
+    (await tokens).find({ username, refresh, timestamp: { $gt: Date.now() - EXP_TIME_MS } }).toArray(async (_err, docs) => {
+        if (docs.length) {
+            resolve(true);
+        } else {
+            resolve(false);
+        }
+    });
+});
+
+const deleteRefreshTokens = (username) => new Promise(async (resolve) => {
+    (await tokens).remove({ username }).then(({ result }) => {
+        resolve(result.n);
+    });
+});
+
 module.exports = {
     getUserAuthInfo,
-    createUserAccount
+    createUserAccount,
+    createRefreshToken,
+    getRefreshToken,
+    deleteRefreshTokens
 };
--- routes/user.js
+++ routes/user.js
@@ -2,25 +2,18 @@ const express = require('express');
 const uuid = require('uuid');
 const router = express.Router();
 
-const { getUserAuthInfo, createUserAccount } = require('../queries/user');
+const { getUserAuthInfo, createUserAccount, createRefreshToken, getRefreshToken, deleteRefreshTokens } = require('../queries/user');
 const { sign, authenticate, credentialsSchema } = require('../utils/routing');
-
-let refreshTokens = [];
-const EXP_TIME_MS = 1 * 60 * 1000;
-
-const clearUserRefreshTokens = username => refreshTokens = refreshTokens.filter(refreshToken => refreshToken.username !== username);
 
 router.use(express.json());
 
 router.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    getUserAuthInfo(username).then((docs) => {
+    getUserAuthInfo(username).then(async (docs) => {
         if (docs.length && docs[0].password === password) {
             const token = sign({ username });
-            const refresh = uuid.v4();
-            clearUserRefreshTokens(username);
-            refreshTokens.push({ username, refresh, timestamp: Date.now() });
+            const refresh = await createRefreshToken(username);
             res.json({ message: 'Successful login!', token, refresh });
         } else {
             res.status(400).json({ error: 'Credentials don\'t match!' })
@@ -28,19 +21,17 @@ router.post('/login', (req, res) => {
     });
 });
 
-router.get('/logout', authenticate, (req, res) => {
-    clearUserRefreshTokens(req.user.username);
+router.get('/logout', authenticate, async (req, res) => {
+    deleteRefreshTokens(req.user.username);
     res.json({ message: 'User is deauthenticated.' });
 });
 
-router.post('/refresh', (req, res) => {
+router.post('/refresh', async (req, res) => {
     const { username, refresh } = req.body;
-    const refreshTokenIndex = refreshTokens.findIndex(refreshToken => refreshToken.refresh === refresh && refreshToken.username == username)
-    if (refreshTokenIndex !== -1 && Date.now() - refreshTokens[refreshTokenIndex].timestamp < EXP_TIME_MS) {
+    const refreshIsValid = await getRefreshToken(username, refresh);
+    if (refreshIsValid) {
         const token = sign({ username });
-        const refresh = uuid.v4();
-        clearUserRefreshTokens(username);
-        refreshTokens.push({ username, refresh });
+        const refresh = await createRefreshToken(username);
         res.json({ message: 'New token successfully obtained.', token, refresh });
     } else {
         res.status(400).send({ error: 'Refresh token is invalid.' });
