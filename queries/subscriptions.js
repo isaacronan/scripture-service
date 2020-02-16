c78a525ceb7567132c6b3425c1db0d50bcc62558
Isaac Ronan <isaacronan@users.noreply.github.com>
2020-02-16 05:58:06 -0500

    updated all queries to use db class functions and return promises

--- queries/subscriptions.js
+++ queries/subscriptions.js
@@ -1,43 +1,40 @@
 const uuid = require('uuid');
-const { getCollection, constructIssuePipeline, constructIssueResponse, constructCurrentIssue } = require('../utils/db');
+const { dbService: { getCollection }, constructIssuePipeline, constructIssueResponse, constructCurrentIssue } = require('../utils/db');
 
-const verses = getCollection('verses');
-const subscriptions = getCollection('subscriptions');
-
-const getCurrentIssue = (subscription) => new Promise(async (resolve, reject) => {
-    (await verses).aggregate(constructIssuePipeline(subscription)).toArray((_err, docs) => {
-        resolve(constructIssueResponse(subscription, docs));
+const getCurrentIssue = (subscription) => getCollection('verses').then((verses) => {
+    return verses.aggregate(constructIssuePipeline(subscription)).toArray().then((docs) => {
+        return constructIssueResponse(subscription, docs);
     });
 });
 
-const getSubscriptions = (username) => new Promise(async (resolve) => {
-    (await subscriptions).find({ username }, { projection: { _id: 0, username: 0 }}).toArray((_err, docs) => {
-        resolve(docs);
+const getSubscriptions = (username) => getCollection('subscriptions').then((subscriptions) => {
+    return subscriptions.find({ username }, { projection: { _id: 0, username: 0 }}).toArray().then((docs) => {
+        return docs;
     });
 });
 
-const getSubscription = (username, id) => new Promise(async (resolve) => {
-    (await subscriptions).findOne({ username, id }, { projection: { _id: 0, username: 0, id: 0 }}).then((doc) => {
-        resolve(doc);
+const getSubscription = (username, id) => getCollection('subscriptions').then((subscriptions) => {
+    return subscriptions.findOne({ username, id }, { projection: { _id: 0, username: 0, id: 0 }}).then((doc) => {
+        return doc;
     });
 });
 
-const updateSubscription = (username, id, subscription) => new Promise(async (resolve) => {
-    (await subscriptions).updateOne({ username, id }, { $set: { ...subscription }}).then(({ result }) => {
-        resolve(result.n);
+const updateSubscription = (username, id, subscription) => getCollection('subscriptions').then((subscriptions) => {
+    return subscriptions.updateOne({ username, id }, { $set: { ...subscription }}).then(({ result }) => {
+        return result.n;
     });
 });
 
-const createSubscription = (username, subscription) => new Promise(async (resolve) => {
+const createSubscription = (username, subscription) => getCollection('subscriptions').then((subscriptions) => {
     const id = uuid.v4();
-    (await subscriptions).insertOne({ username, id, ...subscription, currentIssue: constructCurrentIssue(subscription) }).then(() => {
-        resolve(id);
+    return subscriptions.insertOne({ username, id, ...subscription, currentIssue: constructCurrentIssue(subscription) }).then(() => {
+        return id;
     });
 });
 
-const deleteSubscription = (username, id) => new Promise(async (resolve) => {
-    (await subscriptions).deleteMany({ username, id }).then(({ result }) => {
-        resolve(result.n);
+const deleteSubscription = (username, id) => getCollection('subscriptions').then((subscriptions) => {
+    return subscriptions.deleteMany({ username, id }).then(({ result }) => {
+        return result.n;
     });
 });
 
