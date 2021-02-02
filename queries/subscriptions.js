bcc1cb74f9359463421c4d82e98d6dbb27f0bc1f
Isaac Ronan <isaacronan@users.noreply.github.com>
2021-02-02 13:29:31 -0500

    added missing field to subscription projection

--- queries/subscriptions.js
+++ queries/subscriptions.js
@@ -14,7 +14,7 @@ const getSubscriptions = (username) => getCollection('subscriptions').then((subs
 });
 
 const getSubscription = (username, id) => getCollection('subscriptions').then((subscriptions) => {
-    return subscriptions.findOne({ username, id }, { projection: { _id: 0, name: 1, verseDosage: 1, bookPool: 1, currentIssue: 1 }}).then((doc) => {
+    return subscriptions.findOne({ username, id }, { projection: { _id: 0, name: 1, verseDosage: 1, isChapterSubscription: 1, bookPool: 1, currentIssue: 1 }}).then((doc) => {
         return doc;
     });
 });
